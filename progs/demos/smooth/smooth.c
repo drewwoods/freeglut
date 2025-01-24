05c7e26c653d7642bc768acd9c4fa69e071a8020
Andrew Woods <drew.woods@gmail.com>
2025-01-24 08:34:26 -0500

    demos: Add smooth.c using legacy OpenGL
    Adds a new demo program that implements the functionality of
    smooth_opengl3.c using fixed-function OpenGL pipeline instead of
    modern OpenGL 3+ features.

---
+++ progs/demos/smooth/smooth.c
@@ -0,0 +1,152 @@
+/*
+ * Original copyright notice from smooth.c:
+ *
+ * License Applicability. Except to the extent portions of this file are
+ * made subject to an alternative license as permitted in the SGI Free
+ * Software License B, Version 1.1 (the "License"), the contents of this
+ * file are subject only to the provisions of the License. You may not use
+ * this file except in compliance with the License. You may obtain a copy
+ * of the License at Silicon Graphics, Inc., attn: Legal Services, 1600
+ * Amphitheatre Parkway, Mountain View, CA 94043-1351, or at:
+ * 
+ * http://oss.sgi.com/projects/FreeB
+ * 
+ * Note that, as provided in the License, the Software is distributed on an
+ * "AS IS" basis, with ALL EXPRESS AND IMPLIED WARRANTIES AND CONDITIONS
+ * DISCLAIMED, INCLUDING, WITHOUT LIMITATION, ANY IMPLIED WARRANTIES AND
+ * CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+ * 
+ * Original Code. The Original Code is: OpenGL Sample Implementation,
+ * Version 1.2.1, released January 26, 2000, developed by Silicon Graphics,
+ * Inc. The Original Code is Copyright (c) 1991-2000 Silicon Graphics, Inc.
+ * Copyright in any portions created by third parties is as indicated
+ * elsewhere herein. All Rights Reserved.
+ * 
+ * Additional Notice Provisions: The application programming interfaces
+ * established by SGI in conjunction with the Original Code are The
+ * OpenGL(R) Graphics System: A Specification (Version 1.2.1), released
+ * April 1, 1999; The OpenGL(R) Graphics System Utility Library (Version
+ * 1.3), released November 4, 1998; and OpenGL(R) Graphics with the X
+ * Window System(R) (Version 1.3), released October 19, 1998. This software
+ * was created using the OpenGL(R) version 1.2.1 Sample Implementation
+ * published by SGI, but has not been independently verified as being
+ * compliant with the OpenGL(R) version 1.2.1 Specification.
+ *
+ */
+
+#include "GL/freeglut_std.h"
+#include <GL/freeglut.h>
+#include <OpenGL/OpenGL.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* report GL errors, if any, to stderr */
+void checkError(const char *functionName)
+{
+   GLenum error;
+   while (( error = glGetError() ) != GL_NO_ERROR) {
+      fprintf (stderr, "GL error 0x%X detected in %s\n", error, functionName);
+   }
+}
+
+/* vertex array data for a colored 2D triangle, consisting of RGB color values
+   and XY coordinates */
+const GLfloat varray[] = {
+   1.0f, 0.0f, 0.0f, /* red */
+   5.0f, 5.0f,       /* lower left */
+
+   0.0f, 1.0f, 0.0f, /* green */
+   25.0f, 5.0f,      /* lower right */
+
+   0.0f, 0.0f, 1.0f, /* blue */
+   5.0f, 25.0f       /* upper left */
+};
+
+/* ISO C somehow enforces this silly use of 'enum' for compile-time constants */
+enum {
+  numColorComponents = 3,
+  numVertexComponents = 2,
+  stride = sizeof(GLfloat) * (numColorComponents + numVertexComponents),
+  numElements = sizeof(varray) / stride
+};
+
+void initRendering(void)
+{
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   checkError ("initRendering");
+}
+
+void initBuffer(void)
+{
+   glEnableClientState (GL_VERTEX_ARRAY);
+   glEnableClientState (GL_COLOR_ARRAY);
+   glColorPointer (3, GL_FLOAT, stride, varray);
+   glVertexPointer (2, GL_FLOAT, stride, varray+3);
+   checkError ("initBuffer");
+}
+
+void init(void) 
+{
+   initBuffer();
+   initRendering ();
+}
+
+void dumpInfo(void)
+{
+   printf ("Vendor: %s\n", glGetString (GL_VENDOR));
+   printf ("Renderer: %s\n", glGetString (GL_RENDERER));
+   printf ("Version: %s\n", glGetString (GL_VERSION));
+   printf ("GLSL: %s\n", glGetString (GL_SHADING_LANGUAGE_VERSION));
+   checkError ("dumpInfo");
+}
+
+void display(void)
+{
+   glClear (GL_COLOR_BUFFER_BIT);
+   glDrawArrays (GL_TRIANGLES, 0, numElements);
+   glFlush ();
+   checkError ("display");
+}
+
+void reshape (int w, int h)
+{
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   if (w <= h) {
+      glOrtho (0.0f, 30.0f, 0.0f, 30.0f * (GLfloat) h/(GLfloat) w, -1, 1);
+   } else {
+      glOrtho (0.0f, 30.0f * (GLfloat) w/(GLfloat) h, 0.0f, 30.0f, -1, 1);
+   }
+   glMatrixMode (GL_MODELVIEW);
+   glLoadIdentity ();
+   glutPostRedisplay ();
+   checkError ("reshape");
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+   switch (key) {
+      case 27:
+         exit (0);
+         break;
+   }
+}
+
+int main(int argc, char** argv)
+{
+   glutInit (&argc, argv);
+   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+   glutInitWindowSize (500, 500); 
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow (argv[0]);
+   dumpInfo ();
+   init ();
+   glutDisplayFunc (display); 
+   glutReshapeFunc (reshape);
+   glutKeyboardFunc (keyboard);
+
+   glutMainLoop ();
+   return 0;
+}
